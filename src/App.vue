6fadb5553b5de3946553e8c3ff00945ed92ae06d
anzaldira <anzaldi.aldi@gmail.com>
2023-07-28 22:29:29 -0600

    portofolio

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <NavbarComponent></NavbarComponent>
+    <!-- <cobacoba></cobacoba> -->
+</div>
+
+</template>
+
+<script setup>
+import NavbarComponent from './NavbarComponent.vue';
+// import cobacoba from './cobacoba.vue';
+
+</script>
---
+++ src/NavbarComponent.vue
@@ -0,0 +1,215 @@
+<template>
+    
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center">
+        <!-- <img src="" class="h-8 mr-3" alt="Flowbite Logo" /> -->
+       <center> <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Anzaldi Riyad Al Amari</span> </center>
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Biodata</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+    <!-- <section class="mb-8 bg-center bg-no-repeat bg-[url('https://static.vecteezy.com/system/resources/previews/006/262/477/original/white-abstract-background-backdrop-for-presentation-design-for-website-free-photo.jpg')] bg-white-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56"> -->
+        <br>
+        <br>
+        <br>
+        <br>
+        <center>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl">My Web portofolio</h1>
+        <p class="mb-8 text-lg font-normal text-black-300 lg:text-xl sm:px-16 lg:px-48">
+slide down for more info</p>
+        </center>
+    <!-- </div>
+</section> -->
+</nav>
+<div class="bg-cover bg-center ..." style="background-image: url(...)"></div>
+
+
+
+<!-- <center> -->
+<div class="grid gap-4">
+    <div>
+        <center>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <img class="ml-2 h-auto max-w-full rounded-lg" src="./assets/foto.jpg.jpg" width="400" height="450" align="left" >
+        <div>
+            <br>
+            <br>
+            <br>
+        <div class="grid mb-8 border-0 md:mb-12">
+    <figure class="flex flex-col"><br>
+           <b class="p-2"  style="font-size: 30px; text-align: center; color: black; font-family: 'sans-serif', Calibri, 'Trebuchet MS', sans-serif;">Biodata</b>
+           <center> <div style="width: 50%; background: rgb(122, 121, 118); height: 5px;"></div></center>
+           </figure>
+        </div>
+    </div>
+    <table class="table-auto">
+  <tbody>
+    <tr>
+      <td>Nama </td>
+      <td>:</td>
+      <td>Anzaldi Riyad Al Amari</td>
+    </tr>
+    <tr>
+      <td>Tempat Lahir</td>
+      <td>:</td>
+      <td>Bandung</td>
+    </tr>
+    <tr>
+      <td>Tanggal Lahir</td>
+      <td>:</td>
+      <td>05 Mei 2006</td>
+    </tr>
+    <tr>
+      <td>Alamat</td>
+      <td>:</td>
+      <td>Graha Rancamnayar</td>
+    </tr>
+    <tr>
+      <td>Kelamin</td>
+      <td>:</td>
+      <td>Laki-Laki</td>
+    </tr>
+    <tr>
+      <td>Agama</td>
+      <td>:</td>
+      <td>Islam</td>
+    </tr>
+  </tbody>
+</table>
+
+         </center>
+    </div>
+    <div class="grid grid-cols-5 gap-4">
+    </div>
+</div>
+<!-- </center> -->
+
+<div>
+        <div class="grid mb-8 border-0 md:mb-12">
+    <figure class="flex flex-col"><br>
+           <b class="p-2"  style="font-size: 30px; text-align: center; color: black; font-family: 'sans-serif', Calibri, 'Trebuchet MS', sans-serif;">Keterampilan</b>
+           <center> <div style="width: 50%; background: rgb(122, 121, 118); height: 5px;"></div></center>
+           </figure>
+           <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-3"> 
+<div class="gap-8 sm:grid sm:grid-cols-2">
+
+    <div class="flex justify-between mb-1">
+  <span class="text-base font-medium text-grey-700 dark:text-white">Javascript</span>
+  <span class="text-sm font-medium text-blue-700 dark:text-white"></span>
+</div>
+<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+  <div class="bg-blue-600 h-2.5 rounded-full" style="width: 200%"></div>
+</div>
+
+<div class="flex justify-between mb-1">
+  <span class="text-base font-medium text-grey-700 dark:text-white">CSS</span>
+  <span class="text-sm font-medium text-blue-700 dark:text-white"></span>
+</div>
+<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+  <div class="bg-blue-600 h-2.5 rounded-full" style="width: 300%"></div>
+</div>
+
+<div class="flex justify-between mb-1">
+  <span class="text-base font-medium text-grey-700 dark:text-white">HTML</span>
+  <span class="text-sm font-medium text-blue-700 dark:text-white"></span>
+</div>
+<div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+  <div class="bg-blue-600 h-2.5 rounded-full" style="width: 350%"></div>
+</div>
+
+</div> 
+</div>
+        </div>
+    </div>
+
+    <div>
+        <div class="grid mb-8 border-0 md:mb-12">
+    <figure class="flex flex-col"><br>
+           <b class="p-2"  style="font-size: 30px; text-align: center; color: black; font-family: 'sans-serif', Calibri, 'Trebuchet MS', sans-serif;">Kontak</b>
+           <center> <div style="width: 50%; background: rgb(121, 120, 119); height: 5px;"></div></center>
+           </figure>
+        </div>
+    </div>
+    <div class="mx-auto flex pb-20">
+        <div class="mx-auto" id="kontak">
+            <a href="mailto:anzaldi.aldi@gmail.com@gmail.com">
+        <button class="flex my-2 p-2 bg-blue-600 text-white rounded-md w-full text-left">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope m-auto me-3" viewBox="0 0 16 16">
+         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+        </svg>
+        anzaldi.aldi@gmail.com@gmail.com
+        </button></a>
+        <a href="https://wa.me/+62895426687020">
+        <button class="flex my-2 p-2 bg-green-600 text-white rounded-md w-full text-left">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp me-3" viewBox="0 0 16 16">
+            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+            </svg> 
+            089691512515
+        </button></a>
+        <a href="https://www.instagram.com/mhmmddkaaa_/" class="text-blue-700 hover:underline" target="_blank">
+        <button class="flex my-2 p-2 bg-red-600 text-white rounded-md w-full text-left">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram  me-3" viewBox="0 0 16 16">
+            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+            </svg>
+            @anzaldiriyadalamari
+        </button></a></div>
+    </div>     
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+</footer>
+
+
+
+</template>
